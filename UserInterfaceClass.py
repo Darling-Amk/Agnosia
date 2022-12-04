024d26ae9bbb591af96c4c523531848800a1a727
Darling-Amk <kruchinin.nikita.09@gmail.com>
2022-12-04 17:13:04 +0300

    Add UIClass

--- UserInterfaceClass.py
+++ UserInterfaceClass.py
@@ -3,17 +3,22 @@ from  SETTINGS import  MAIN_MENU_FONT,MAIN_MENU_FONT_LOGO
 import pygame
 
 class Scene:
-    def __init__(self,screen,name,bg_image=""):
-        self.bg = pygame.image.load("Agnosia_assets/Agnosia_background_main_menu.png")
+    def __init__(self,screen,name,change,bg_image="Agnosia_assets/Agnosia_background_main_menu.png"):
+        self.bg = pygame.image.load(bg_image)
+        self.change = change
         self.name = name
         self.screen = screen
     def draw(self):
         pass
 
+    @staticmethod
+    def draw_text(surface, text, x, y, font, color=(255, 255, 255)):
+        surface.blit(font.render(f'{text}', True, color), (x, y))
+
 class MainMenu(Scene):
 
-    def __init__(self,screen,name,bg_image=""):
-        super. __init__(screen,name,bg_image)
+    def __init__(self,screen,name,change,bg_image):
+        super().__init__(screen,name,change,bg_image)
 
         self.btn1 = Button(360, 80, None, (0, 0, 0), screen, MAIN_MENU_FONT)
         self.btn2 = Button(275, 80, None, (0, 0, 0), screen, MAIN_MENU_FONT)
@@ -23,24 +28,41 @@ class MainMenu(Scene):
     def draw(self):
         self.screen.fill((255, 0, 0))
         self.screen.blit(self.bg, (0, 0))
-        draw_text(self.screen, "Agnosia", 630, 247, MAIN_MENU_FONT_LOGO, color=(0, 0, 0))
-        self.btn1.draw(764, 540, "New game", lambda: change(all_scenes["Map"]))
-        self.btn2.draw(808, 667, "Tutorial", lambda: change(None))
-        self.btn3.draw(808, 796, "Option", lambda: change(all_scenes["Options"]))
-        self.btn4.draw(877, 925, "Exit", lambda: change(None))
+        Scene.draw_text(self.screen, "Agnosia", 630, 247, MAIN_MENU_FONT_LOGO, color=(0, 0, 0))
+        self.btn1.draw(764, 540, "New game", lambda: self.change("Map"))
+        self.btn2.draw(808, 667, "Tutorial", lambda: self.change("None"))
+        self.btn3.draw(808, 796, "Option", lambda: self.change("Options"))
+        self.btn4.draw(877, 925, "Exit", lambda: self.change("None"))
+        print("Menu draw!")
+        pygame.display.flip()
+
 
 
 
 class UserInterface:
     def __init__(self,screen):
         self.screen = screen
-        self.scenes = {}
-        self.current_scene = None
+        self.scenes = {"None":False}
+        self.current_scene = "Menu"
+        self.CreateMainMenu()
 
-    def draw(self,state):
-        self.scenes[self.current_scene].draw()
+
+    def change(self,scene):
+        self.current_scene = self.scenes[scene]
+
+
+    def draw(self,state={}):
+        if self.current_scene:
+            self.scenes[self.current_scene].draw()
+
+        return bool(self.current_scene)
 
     def CreateMainMenu(self):
-        self.scenes['Menu'] = Scene("Menu")
 
-    def drawMainMenu(self):
+        self.scenes['Menu'] = MainMenu(
+            screen=self.screen ,
+            name="Menu",
+            bg_image="Agnosia_assets/Agnosia_background_main_menu.png",
+            change = self.change
+        )
+
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import time
 from Menu import  MenuScene
 from Map import MapScene
 from Options import OptionsScene
+
+from UserInterfaceClass import UserInterface
+
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 current_scene = [None]
@@ -13,10 +16,24 @@ all_scenes = {
 }
 current_scene[0] = MenuScene
 
-while current_scene[0] is not None:
-    current_scene[0](screen,current_scene,clock,all_scenes)
-    time.sleep(.1)
-    print("Изменилась сцена")
+UI = UserInterface(screen)
+play = True
+while play:
+    play = UI.draw()
+    clock.tick(10)
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            play = False
+
+
+
+# while current_scene[0] is not None:
+#     current_scene[0](screen,current_scene,clock,all_scenes)
+#     time.sleep(.1)
+#     print("Изменилась сцена")
+
+
+
 
 pygame.quit()
 
