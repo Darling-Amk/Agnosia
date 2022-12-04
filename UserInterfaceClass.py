97eeec5bd18bceed5383547d45cbcc72dd39f69b
Darling-Amk <kruchinin.nikita.09@gmail.com>
2022-12-04 17:25:01 +0300

    Add Options

--- UserInterfaceClass.py
+++ UserInterfaceClass.py
@@ -16,7 +16,6 @@ class Scene:
         surface.blit(font.render(f'{text}', True, color), (x, y))
 
 class MainMenu(Scene):
-
     def __init__(self,screen,name,change,bg_image):
         super().__init__(screen,name,change,bg_image)
 
@@ -36,7 +35,18 @@ class MainMenu(Scene):
         print("Menu draw!")
         pygame.display.flip()
 
+class Options(Scene):
+    def __init__(self,screen,name,change,bg_image):
+        super().__init__(screen,name,change,bg_image)
+        self.btn = Button(190, 80, None, (0, 0, 0), screen, font=MAIN_MENU_FONT)
 
+    def draw(self):
+        self.screen.fill((255, 0, 0))
+        self.screen.blit(self.bg, (0, 0))
+        # For drawing
+        Scene.draw_text(self.screen, "Agnosia", 630, 247, MAIN_MENU_FONT_LOGO, color=(0, 0, 0))
+        self.btn.draw(877, 925, "Back", lambda: self.change("Menu"))
+        pygame.display.flip()
 
 
 class UserInterface:
@@ -45,23 +55,29 @@ class UserInterface:
         self.scenes = {"None":False}
         self.current_scene = "Menu"
         self.CreateMainMenu()
-
+        self.CreateOptions()
 
     def change(self,scene):
-        self.current_scene = self.scenes[scene]
-
+        self.current_scene = scene
 
     def draw(self,state={}):
         if self.current_scene:
+            print(self.current_scene)
             self.scenes[self.current_scene].draw()
 
         return bool(self.current_scene)
 
     def CreateMainMenu(self):
-
         self.scenes['Menu'] = MainMenu(
             screen=self.screen ,
             name="Menu",
+            bg_image="Agnosia_assets/Agnosia_background_main_menu.png",
+            change = self.change
+        )
+    def CreateOptions(self):
+        self.scenes['Options'] = Options(
+            screen=self.screen ,
+            name="Options",
             bg_image="Agnosia_assets/Agnosia_background_main_menu.png",
             change = self.change
         )
--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from SETTINGS import *
-import time
-from Menu import  MenuScene
-from Map import MapScene
-from Options import OptionsScene
-
 from UserInterfaceClass import UserInterface
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
-current_scene = [None]
-all_scenes = {
-    "Menu":MenuScene,
-    "Map":MapScene,
-    "Options":OptionsScene
-}
-current_scene[0] = MenuScene
 
 UI = UserInterface(screen)
 play = True
@@ -24,15 +12,6 @@ while play:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             play = False
-
-
-
-# while current_scene[0] is not None:
-#     current_scene[0](screen,current_scene,clock,all_scenes)
-#     time.sleep(.1)
-#     print("Изменилась сцена")
-
-
 
 
 pygame.quit()
